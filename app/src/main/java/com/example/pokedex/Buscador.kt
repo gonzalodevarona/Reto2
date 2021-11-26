ef7e654cc2a666406fb147db4771174798020845
gonzalodevarona <Billonaire24>
2021-11-25 19:53:39 -0500

    success on passing extras from mainactivity to buscador activity

--- app/src/main/java/com/example/pokedex/Buscador.kt
+++ app/src/main/java/com/example/pokedex/Buscador.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.lifecycle.lifecycleScope
 import com.example.pokedex.databinding.ActivityBuscadorBinding
 import com.example.pokedex.databinding.ActivityMainBinding
+import com.example.pokedex.util.Constants
+import com.example.pokedex.util.HTTPSWebUtilDomi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Buscador : AppCompatActivity() {
 
     private lateinit var binding: ActivityBuscadorBinding
+
+    private lateinit var username: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,20 @@ class Buscador : AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         supportActionBar?.hide()
         setContentView(binding.root)
+
+        username = intent.extras?.getString("username")!!
+
+        
+
+        binding.catchBtn.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                HTTPSWebUtilDomi().GETRequest("${Constants.BASE_URL_POKEAPI}/${binding.catchPokemon.text}")
+            }
+        }
+
+
+
+
     }
 
 } //end of class
